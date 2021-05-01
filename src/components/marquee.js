13e4036c33edb31ff80b26f0f54819383a9af7f1
holdmypotion <holdmypotion@gmail.com>
2021-05-01 12:19:20 +0530

    Well It's done

---
+++ src/components/marquee.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { motion } from "framer-motion";
+
+import "./marquee.css";
+
+const marqueeVariants = {
+  animate: {
+    x: [0, -1035],
+    transition: {
+      x: {
+        repeat: Infinity,
+        repeatType: "loop",
+        duration: 5,
+        ease: "linear",
+      },
+    },
+  },
+};
+
+const Marquee = () => {
+  return (
+    <div>
+      <div className="marquee">
+        <motion.div
+          className="track"
+          variants={marqueeVariants}
+          animate="animate"
+        >
+          <h1>
+            Let's Work Together. Let's Work Together. Let's Work Together. Let's
+            Work Together. Let's Work Together. Let's Work Together. Let's Work
+            Together
+          </h1>
+        </motion.div>
+      </div>
+    </div>
+  );
+};
+
+export default Marquee;
